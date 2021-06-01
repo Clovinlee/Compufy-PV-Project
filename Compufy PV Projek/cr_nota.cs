4a26d1cb43766a2ff9536eed1bf193c7bcd7adaa
Chrisanto Sinatra <chrisantosinatra6@gmail.com>
2021-06-01 21:31:55 +0700

    set tanggal to DATETIME not DATE

--- Compufy PV Projek/cr_nota.cs
+++ Compufy PV Projek/cr_nota.cs
@@ -119,6 +119,14 @@ namespace Compufy_PV_Projek {
                 return this.DataDefinition.ParameterFields[3];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_PDatetime {
+            get {
+                return this.DataDefinition.ParameterFields[4];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
