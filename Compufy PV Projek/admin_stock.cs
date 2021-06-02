f9cd79cbb5162934415f9ea0cd86a18c90fc0a85
Chrisanto Sinatra <chrisantosinatra6@gmail.com>
2021-06-02 08:33:32 +0700

    test1

--- Compufy PV Projek/admin_stock.cs
+++ Compufy PV Projek/admin_stock.cs
@@ -109,7 +109,8 @@ namespace Compufy_PV_Projek
             frmUpdate.id = id;
             frmUpdate.kategori = kategori;
             frmUpdate.txtNama.Text = nama;
-            frmUpdate.txtHarga.Text = Convert.ToString(double.Parse(harga, NumberStyles.Currency));
+            //frmUpdate.txtHarga.Text = Convert.ToString(double.Parse(harga, NumberStyles.Currency));
+            frmUpdate.txtHarga.Text = "Rp "+Convert.ToDouble(harga).ToString("#,##");
             frmUpdate.txtStok.Text = arrStok[0];
             frmUpdate.ShowDialog();
             dataGridView1.Rows.Clear();
